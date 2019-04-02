2de53f678156a5ae4a14641957a2ea9ba019bb72
a18wilis <a18wilis@student.his.se>
2019-04-02 08:48:13 +0200

    added donk

--- app/src/main/java/com/example/brom/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/brom/webviewapp/MainActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+//donk
 public class MainActivity extends AppCompatActivity {
     // Create a private member variable that can hold our WebView
 
