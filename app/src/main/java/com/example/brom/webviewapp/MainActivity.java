3e00d367e4633bec6bc04e71b0387cdbcd8d4e86
a18wilis <a18wilis@student.his.se>
2019-04-04 14:12:15 +0200

    Lade till setContentView (webView)

--- app/src/main/java/com/example/brom/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/brom/webviewapp/MainActivity.java
@@ -24,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
         WebView webView = new WebView(this);
 
+        setContentView(webView);
+
+
         // The FAB-code can be removed
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -33,17 +36,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        // 1. Create a WebView element in the layout file content_main.xml
-        // -- Commit and push to your github fork
-        // 2. Give the WebView element created in step 1 ID "my_webview"
-        // Enter your code that creates your WebView here...
-        // -- Commit and push to your github fork
-        // 3. Locate the WebView element created in step 1 using the ID created in step 2
-        // 4. Create a new WebViewClient to attach to our WebView. This allows us to
-        //    browse the web inside our app.
-        // -- Commit and push to your github fork
-        // 5. Enter the url to load in our WebView
-        // -- Commit and push to your github fork
     }
 
     @Override
